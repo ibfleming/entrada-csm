d3256e62e298caf4a54cc75ea4c0d39ec08eec30
ibfleming <ianfleming678@gmail.com>
2024-08-18 23:11:47 -0700

    🎉 Init!

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["ver2-entrada_*"],
+} satisfies Config;
