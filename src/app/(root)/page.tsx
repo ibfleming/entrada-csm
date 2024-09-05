49b961cb57a0869eac3793caf334b3ce3b9fb9b6
ibfleming <ianfleming678@gmail.com>
2024-09-04 23:43:50 -0700

    🔨 Making custom table now...

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -18,54 +18,61 @@ import {
   SecondaryButton,
 } from "@/custom/buttons";
 import { Input } from "@/ui/input";
-import Checkbox from "@/custom/checkbox";
+import { Checkbox, CheckboxWithText } from "@/custom/checkbox";
+import { ResidentTable } from "@/custom/tables/resident";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-start gap-4 p-4">
-      <PrimaryButton>Primary</PrimaryButton>
-      <SecondaryButton>Secondary</SecondaryButton>
-      <DestructiveButton>Destructive</DestructiveButton>
-      <OutlineButton>Outline</OutlineButton>
-      <GhostButton>Ghost</GhostButton>
-      <LinkButton>Link</LinkButton>
-      <IconButton>
-        <FileTerminal className="h-4 w-4" />
-      </IconButton>
-      <ButtonIconText icon={Mail}>Login</ButtonIconText>
-      <Checkbox id="terms" label="Accept terms and conditions" />
+    <div>
+      <div className="flex items-center justify-start gap-4 p-4">
+        <PrimaryButton>Primary</PrimaryButton>
+        <SecondaryButton>Secondary</SecondaryButton>
+        <DestructiveButton>Destructive</DestructiveButton>
+        <OutlineButton>Outline</OutlineButton>
+        <GhostButton>Ghost</GhostButton>
+        <LinkButton>Link</LinkButton>
+        <IconButton>
+          <FileTerminal className="h-4 w-4" />
+        </IconButton>
+        <ButtonIconText icon={Mail}>Login</ButtonIconText>
+        <CheckboxWithText id="terms" label="Accept terms and conditions" />
+        <Checkbox />
 
-      <div className="w-56">
-        <Input
-          className="border-2 font-inter text-neutral-800 shadow-md focus-visible:ring-2"
-          type="email"
-          placeholder="Email"
-        />
+        <div className="w-56">
+          <Input
+            className="border-2 font-inter text-neutral-800 shadow-md focus-visible:ring-2"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger className="focus-visible:outline-none">
+            <span className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md bg-primary px-4 py-2 font-inter text-sm font-medium text-primary-foreground shadow transition-colors focus-within:ring-transparent hover:bg-hover focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
+              Dropdown
+            </span>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="font-rubik text-primary shadow-lg">
+            <DropdownMenuLabel className="text-xs">
+              My Account
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="text-xs hover:cursor-pointer hover:text-primary focus:text-primary">
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-xs hover:cursor-pointer hover:text-primary focus:text-primary">
+              Billing
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-xs hover:cursor-pointer hover:text-primary focus:text-primary">
+              Team
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-xs hover:cursor-pointer hover:text-primary focus:text-primary">
+              Subscription
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <ResidentTable />
       </div>
-
-      <DropdownMenu>
-        <DropdownMenuTrigger className="focus-visible:outline-none">
-          <span className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md bg-primary px-4 py-2 font-inter text-sm font-medium text-primary-foreground shadow transition-colors focus-within:ring-transparent hover:bg-hover focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50">
-            Dropdown
-          </span>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="font-rubik text-primary shadow-lg">
-          <DropdownMenuLabel className="text-xs">My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className="text-xs hover:cursor-pointer hover:text-primary focus:text-primary">
-            Profile
-          </DropdownMenuItem>
-          <DropdownMenuItem className="text-xs hover:cursor-pointer hover:text-primary focus:text-primary">
-            Billing
-          </DropdownMenuItem>
-          <DropdownMenuItem className="text-xs hover:cursor-pointer hover:text-primary focus:text-primary">
-            Team
-          </DropdownMenuItem>
-          <DropdownMenuItem className="text-xs hover:cursor-pointer hover:text-primary focus:text-primary">
-            Subscription
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
     </div>
   );
 }
--- src/components/custom/checkbox.tsx
+++ src/components/custom/checkbox.tsx
@@ -1,15 +1,20 @@
+"use client";
+
+import { cn } from "~/lib/utils";
 import React from "react";
 import { Label } from "@/ui/label";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { CheckIcon } from "@radix-ui/react-icons";
 import { Checkbox as RadixCheckbox } from "@/ui/checkbox";
 
 interface CheckboxProps {
-  id: string;
+  id?: string;
   label?: string;
   className?: string;
   disabled?: boolean;
 }
 
-export default function Checkbox({
+function CheckboxWithText({
   id,
   label,
   className,
@@ -31,3 +36,26 @@ export default function Checkbox({
     </div>
   );
 }
+
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      "peer h-4 max-h-4 min-h-4 w-4 min-w-4 max-w-4 shrink rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      className,
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator
+      className={cn("flex items-center justify-center text-current")}
+    >
+      <CheckIcon className="h-4 w-4" />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox, CheckboxWithText };
